56277fb3e4e9b51a0febef3dd3d07b8f13fca1b2
Ziad Alezzi <lucirieko@gmail.com>
2025-08-23 13:50:27 +0300

    Used breadth first search to solve a graph traversal problem

--- knight.rb
+++ knight.rb
@@ -1,9 +1,25 @@
-def knight_moves(position, destination, queue=[], visited=[])
+def knight_moves(position, destination)
 
-  # Checks all possible moves from location
-  x, y = position
-  queue.append([x+2, y+1], [x-2, y-1], [x+1, y+2], [x-1, y-2], [x-2, y+1], [x-1, y+2], [x+2, y-1], [x+1, y-2])
-  queue = queue.select { |location| (0..7).cover?(location[0]) && (0..7).cover?(location[1]) && !visited.include?(location)}
+  # Initialize the queue and previously visited arrays
+  queue, visited = [], []
 
-  
+  queue << [position]                                     
+
+  while !queue.empty?
+    # Checks all possible moves from location
+    path = queue.shift
+    current = path[-1]
+    return path if current == destination
+
+    x, y = current
+    moves = [[x+2, y+1], [x-2, y-1], [x+1, y+2], [x-1, y-2], [x-2, y+1], [x-1, y+2], [x+2, y-1], [x+1, y-2]]
+    moves = moves.select { |location| (0..7).cover?(location[0]) && (0..7).cover?(location[1]) && !visited.include?(location)}
+
+    moves.each do |move|
+      visited << move
+      queue << (path + [move])
+    end
+  end
 end
+
+p knight_moves([0, 0], [5, 2])
